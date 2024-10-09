f6080610e50b2b2ba2dfdcf4ae4b60a2bfcaeeb9
Witut2005 <wiktor.glogowski@onet.pl>
2024-10-09 20:02:34 +0200

    update

--- interlan-ai-tests/src/app/home/home.page.ts
+++ interlan-ai-tests/src/app/home/home.page.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 import { environment } from 'src/environments/environment.prod';
 
 enum Operations {
-  CreateGrid = 'CREATE',
-  GridAdd = 'ADD',
+  CreateGrid = 'CREATE_GRID',
+  GridAdd = 'ADD_ROW',
 }
 
 @Component({
@@ -13,6 +13,8 @@ enum Operations {
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  Operations = Operations;
+
   columnNames: string[] = [];
   rows: any[] = [];
 
@@ -23,26 +25,41 @@ export class HomePage {
 
   examples: { [key: string]: string } = {
     [Operations.CreateGrid]:
-      "operation' key should be set to 'CREATE' and 'properties' key to ['name', 'age', 'height']",
+      "Declare 'properties' key and set it to ['name', 'age', 'height']",
     [Operations.GridAdd]:
-      "set 'operation' key to 'ADD',  'name' key to 'nicho', 'age' key to 20 and 'height' key to 200",
+      "Set 'name' to 'nicho', 'age' to 20 and 'height' to 200",
   };
 
   ngModelPrompt: string = '';
-
   ngModelOperation: string = Operations.CreateGrid;
-
   waitingForResponse: boolean = false;
 
   constructor(private http: HttpClient) {}
 
+  getPromptBeginning() {
+    const b =
+      "Create a json object. Use '```json' at the begging of json and '```' at the end.";
+
+    if (
+      this.ngModelOperation == Operations.CreateGrid ||
+      this.ngModelOperation == Operations.GridAdd
+    ) {
+      return `${b}. Set 'operation' key to '${this.ngModelOperation}'. `;
+    } else {
+      throw new Error('Unknown operation');
+    }
+  }
+
   sendPrompt() {
+    console.log(`${this.getPromptBeginning()}${this.ngModelPrompt}`);
+
+    if (this.ngModelPrompt.length == 0) {
+    }
+
     this.waitingForResponse = true;
     this.http
       .post(environment.llamaCppBackendIp, {
-        prompt:
-          "Use '```json' at the begging of json and '```' at the end. Create a json object." +
-          this.ngModelPrompt,
+        prompt: `${this.getPromptBeginning()}${this.ngModelPrompt}`,
         n_predict: 128,
       })
       .subscribe((data: any) => {
@@ -67,7 +84,10 @@ export class HomePage {
   }
 
   log() {
-    console.log(this.columnNames, this.rows);
+    console.group();
+    console.log('columns', this.columnNames);
+    console.log('rows', this.rows);
+    console.groupEnd();
   }
 
   handleOperation(json: any) {
